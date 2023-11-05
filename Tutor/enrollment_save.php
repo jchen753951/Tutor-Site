a256508abd676a5878f847e3065c9d5c2becd037
jchen753951 <124069788+jchen753951@users.noreply.github.com>
2023-11-05 13:06:17 -0500

    Add files via upload
    Latest functional Database PHP scripts.

--- Tutor/enrollment_save.php
+++ Tutor/enrollment_save.php
@@ -1,73 +1,38 @@
 <?php
 include 'server.php';
 
-if (
-    isset($_POST['sid']) &&
-    isset($_POST['start-time']) &&
-    isset($_POST['duration']) &&
-    isset($_POST['subject']) &&
-    isset($_POST['date']) &&
-    isset($_POST['tid']) &&
-    isset($_POST['homework-space']) &&
-    isset($_POST['comments-space']) &&
-    isset($_POST['behavior']) &&
-    isset($_POST['motivation']) &&
-    isset($_POST['focus'])
-) {
-    function validate($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $session_id = uniqid();
+    
+    $student_id = $_POST['student_id'];
+    $start_time = $_POST['start_time'];
+    $date = $_POST['date'];
+    $tutor_id = $_POST['tutor_id'];
+    $duration = $_POST['duration'];
+    $subject = $_POST['subject'];
+    $confidence = $_POST['confidence'];
+    $motivation = $_POST['motivation'];
+    $focus = $_POST['focus'];
+    $behavior = $_POST['behavior'];
+    $assigned_homework = $_POST['assigned_homework'];
+    $comments = $_POST['comments'];
 
-    $student_id = validate($_POST['sid']);
-    $session_id = validate($_POST['start-time']);
-    $duration = validate($_POST['duration']);
-    $subject = validate($_POST['subject']);
-    $date = validate($_POST['date']);
-    $tutor_id = validate($_POST['tid']);
-    $homework = validate($_POST['homework-space']);
-    $comments = validate($_POST['comments-space']);
-    $behavior = validate($_POST['behavior']);
-    $motivation = validate($_POST['motivation']);
-    $focus = validate($_POST['focus']);
+    $enrollmentSQL = "INSERT INTO enrollment (S_ID, Session_ID, Homework, Comments, Behavior, Motivation, Confidence, Focus) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt1 = $db->prepare($enrollmentSQL);
+    $stmt1->bind_param("iissiiii", $student_id, $session_id, $assigned_homework, $comments, $behavior, $motivation, $confidence, $focus);
 
-    if (
-        !empty($student_id) &&
-        !empty($session_id) &&
-        !empty($duration) &&
-        !empty($subject) &&
-        !empty($date) &&
-        !empty($tutor_id) &&
-        !empty($homework) &&
-        !empty($comments) &&
-        !empty($behavior) &&
-        !empty($motivation) &&
-        !empty($focus)
-    ) {
+    $sessionSQL = "INSERT INTO session (Session_ID, Start_Time, Length, Subject, Date, T_ID) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt2 = $db->prepare($sessionSQL);
+    $stmt2->bind_param("issssi", $session_id, $start_time, $duration, $subject, $date, $tutor_id);
 
-        // Use prepared statements to insert data into the 'session' table
-        $sessionSql = "INSERT INTO session (Session_ID, Start_Time, Length, Subject, Date, T_ID) VALUES (?, ?, ?, ?, ?, ?)";
-        $sessionStmt = mysqli_prepare($db, $sessionSql);
-        mysqli_stmt_bind_param($sessionStmt, "ssisii", $session_id, $start_time, $duration, $subject, $formattedDate, $tutor_id);
-        $sessionResult = mysqli_stmt_execute($sessionStmt);
-
-        // Use prepared statements to insert data into the 'enrollment' table
-        $enrollmentSql = "INSERT INTO enrollment (S_ID, Session_ID, Homework, Comments, Behavior, Motivation, Confidence, Focus) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        $enrollmentStmt = mysqli_prepare($db, $enrollmentSql);
-        mysqli_stmt_bind_param($enrollmentStmt, "ssssssss", $student_id, $session_id, $homework, $comments, $behavior, $motivation, $confidence, $focus);
-        $enrollmentResult = mysqli_stmt_execute($enrollmentStmt);
-
-        if ($sessionResult && $enrollmentResult) {
-            echo "Enrollment data saved successfully!";
-        } else {
-            echo "Error: " . mysqli_error($db);
-        }
+    if ($stmt1->execute() && $stmt2->execute()) {
+        echo "Data saved successfully! <a href='index.html'>Go back to index</a>";
     } else {
-        echo "Please fill in all the required fields.";
+        echo "Error: " . $db->error;
     }
+
+    $stmt1->close();
+    $stmt2->close();
 } else {
-    echo "Invalid request or missing fields.";
+    header("Location: index.html");
 }
-?>
--- Tutor/student_reg.php
+++ Tutor/student_reg.php
@@ -1,49 +1,34 @@
 <?php
 include 'server.php';
 
-if (isset($_POST['fname']) && isset($_POST['minit']) && isset($_POST['lname']) 
-&& isset($_POST['satdate']) && isset($_POST['glevel']) && isset($_POST['dob']) 
-&& isset($_POST['gfname']) && isset($_POST['gminit']) && isset($_POST['glname'])) {
-    function validate($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $fname = $_POST['fname'];
+    $minit = $_POST['minit'];
+    $lname = $_POST['lname'];
+    $satdate = $_POST['satdate'];
+    $glevel = $_POST['glevel'];
+    $dob = $_POST['dob'];
+    $gfname = $_POST['gfname'];
+    $gminit = $_POST['gminit'];
+    $glname = $_POST['glname'];
+    $phone = $_POST['phone'];
 
-    $first_name = validate($_POST['fname']);
-    $middle_initial = validate($_POST['minit']);
-    $last_name = validate($_POST['lname']);
-    $sat_date = validate($_POST['satdate']);
-    $grade_level = validate($_POST['glevel']);
-    $dob = validate($_POST['dob']);
-    $guardian_first_name = validate($_POST['gfname']);
-    $guardian_middle_initial = validate($_POST['gminit']);
-    $guardian_last_name = validate($_POST['glname']);
+    $guardianSQL = "INSERT INTO guardian (FName, MINIT, LName, phone_no, S_ID) VALUES (?, ?, ?, ?, LAST_INSERT_ID())";
+    $stmt1 = $db->prepare($guardianSQL);
+    $stmt1->bind_param("sssi", $gfname, $gminit, $glname, $phone);
 
-    if (
-        !empty($first_name) &&
-        !empty($middle_initial) &&
-        !empty($last_name) &&
-        !empty($sat_date) &&
-        !empty($grade_level) &&
-        !empty($dob) &&
-        !empty($guardian_first_name) &&
-        !empty($guardian_middle_initial) &&
-        !empty($guardian_last_name)
-    ) {
-        $sql = "INSERT INTO student (FName, MINIT, LName, SAT_date, Grade, DOB, G_FName, G_Minit, G_LName) VALUES ('$first_name', '$middle_initial', '$last_name', '$sat_date', '$grade_level', '$dob', '$guardian_first_name', '$guardian_middle_initial', '$guardian_last_name')";
-        $result = mysqli_query($db, $sql);
+    $studentSQL = "INSERT INTO student (FName, MINIT, LName, SAT_date, Grade, DOB, G_FName, G_Minit, G_LName) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt2 = $db->prepare($studentSQL);
+    $stmt2->bind_param("ssssissss", $fname, $minit, $lname, $satdate, $glevel, $dob, $gfname, $gminit, $glname);
 
-        if ($result) {
-            echo "Student information saved successfully!";
-        } else {
-            echo "Error: " . mysqli_error($db);
-        }
+    if ($stmt1->execute() && $stmt2->execute()) {
+        echo "Student and guardian data saved successfully! <a href='index.html'>Go back to index</a>";
     } else {
-        echo "Please fill in all the required fields.";
+        echo "Error: " . $db->error;
     }
+
+    $stmt1->close();
+    $stmt2->close();
 } else {
-    echo "Invalid request or missing fields.";
+    header("Location: index.html");
 }
-?>
