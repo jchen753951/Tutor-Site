a6987ef65ff35dff828a3d69f69588c95f32d917
Michelle Curry <michellelicurry@icloud.com>
2023-11-30 13:40:48 -0500

    added login functionality

--- Tutor/firebaseAuth.js
+++ Tutor/firebaseAuth.js
@@ -55,6 +55,33 @@ var firebaseConfig = {
        })
      }
 
+     function login () {
+      // Get all our input fields
+      email = document.getElementById('email').value
+      password = document.getElementById('password').value
+    
+      // Validate input fields
+      if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!')
+        return
+        // Don't continue running the code
+      }
+    
+      auth.signInWithEmailAndPassword(email, password)
+      .then(function() {
+        // Declare user variable
+        var user = auth.currentUser
+        alert('User Logged In!!')  
+      })
+      .catch(function(error) {
+        // Firebase will use this to alert of its errors
+        var error_code = error.code
+        var error_message = error.message
+    
+        alert(error_message)
+      })
+    }
+
      function validate_email(email) {
         expression = /^[^@]+@\w+(\.\w+)+\w$/
         if (expression.test(email) == true) {
