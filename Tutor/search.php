9f3194cfdb7a1cd36953cb81e8d5a39288d943c9
jchen753951 <124069788+jchen753951@users.noreply.github.com>
2023-12-04 19:53:21 -0500

    added login without api

--- Tutor/search.php
+++ Tutor/search.php
@@ -64,6 +64,7 @@
             }
 
             $db = mysqli_connect('localhost', 'root', '', 'c2education');
+            //$db = mysqli_connect('localhost', 'id21622081_username', 'P@ssw0rd', 'id21622081_c2education');
 
             if (!$db) {
                 die("Connection failed: " . mysqli_connect_error());
--- Tutor/server.php
+++ Tutor/server.php
@@ -2,6 +2,7 @@
 session_start();
 
 $db = mysqli_connect('localhost', 'root', '', 'c2education');
+//$db = mysqli_connect('localhost', 'id21622081_username', 'P@ssw0rd', 'id21622081_c2education');
 
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
